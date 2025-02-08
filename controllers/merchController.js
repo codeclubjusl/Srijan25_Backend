9e49fa93ced444565d144d9741b9760c3191ccb6
Arnob-B <arnobbhakta00@gmail.com>
2025-02-09 04:16:31 +0530

    adding post route

--- controllers/merchController.js
+++ controllers/merchController.js
@@ -1,59 +1,59 @@
 function MerchController(database) {
 
-    this.database = database
+  this.database = database
 
-    const CONST = require("../utils/constants")
-    const BigPromise = require("../middlewares/bigPromise")
+  const CONST = require("../utils/constants")
+  const BigPromise = require("../middlewares/bigPromise")
 
 
-    this.getMerchant = BigPromise(async (req, res, next) => {
-        const jwtToken = req.cookies.jwt;
-        const {email} = decodeJWT(jwtToken);
-        const {merchandise} = await this.database.getUserByEmail(email);
-        if(merchandise){
-            res.status(CONST.httpStatus.OK);
-            res.json({
-                success:true,
-                merchandise : merchandise
-            });
-        }
-        else{
-            res.status(CONST.httpStatus.OK);
-            res.json({
-                success:false,
-                merchandise: null
-            })
-        }
-    });
-    this.bookMerchant = BigPromise(async (req, res, next) => {
-        const {size, color} = req.body;
-        if(!size || !color){
-            res.status(CONST.httpStatus.BAD_REQUEST);
-            res.json({
-                success:false,
-                message:"missing fields",
-            });
-        }
-        const jwtToken = req.cookies.jwt;
-        const {email} = decodeJWT(jwtToken);
-        const {merchandise} = await this.database.getUserByEmail(email);
-        if(merchandise){
-            res.status(CONST.httpStatus.OK);
-            res.json({
-                success:true,
-                message:"merchandise already added",
-                merchandise : merchandise
-            });
-        }
-        else{
-            await this.database.addMerchToUser(email , size,color);
-            res.status(CONST.httpStatus.CREATED);
-            res.json({
-                success:true,
-                message: "merchandise added"
-            })
-        }
-    });
+  this.getMerchant = BigPromise(async (req, res, next) => {
+    const jwtToken = req.cookies.jwt;
+    const { email } = decodeJWT(jwtToken);
+    const { merchandise } = await this.database.getUserByEmail(email);
+    if (merchandise) {
+      res.status(CONST.httpStatus.OK);
+      res.json({
+        success: true,
+        merchandise: merchandise
+      });
+    }
+    else {
+      res.status(CONST.httpStatus.OK);
+      res.json({
+        success: false,
+        merchandise: null
+      })
+    }
+  });
+  this.bookMerchant = BigPromise(async (req, res, next) => {
+    const { size, color } = req.body;
+    if (!size || !color) {
+      res.status(CONST.httpStatus.BAD_REQUEST);
+      res.json({
+        success: false,
+        message: "missing fields",
+      });
+    }
+    const jwtToken = req.cookies.jwt;
+    const { email } = decodeJWT(jwtToken);
+    const { merchandise } = await this.database.getUserByEmail(email);
+    if (merchandise) {
+      res.status(CONST.httpStatus.OK);
+      res.json({
+        success: true,
+        message: "merchandise already added",
+        merchandise: merchandise
+      });
+    }
+    else {
+      await this.database.addMerchToUser(email, size, color);
+      res.status(CONST.httpStatus.CREATED);
+      res.json({
+        success: true,
+        message: "merchandise added"
+      })
+    }
+  });
 
 }
 

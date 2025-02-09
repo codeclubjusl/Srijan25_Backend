08d15941589e970bcfa441c9f22bfcadae3d0995
Atmik Goswami <atmikcodes@gmail.com>
2025-02-09 14:03:51 +0530

    change getMerchandise route

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const merchController = require("../controllers/merchController")
 router.get("/users/:id", isUserAuthenticated, userController.getUserById)
 router.delete("/users/:id", isUserAuthenticated, userController.deleteUserById)
 router.delete("/users/:id", isUserAuthenticated, userController.deleteUserById)
-router.post("/users/getMerchandise", isUserAuthenticated, merchController.getMerchant)
+router.get("/users/get/Merchandise", isUserAuthenticated, merchController.getMerchant)
 router.post("/users/merchandise", isUserAuthenticated, merchController.bookMerchant)
 
 module.exports = router
